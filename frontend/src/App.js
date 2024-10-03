8604a80c003073041341b1eb9e252589691f8eab
vinitpatel135 <124019126+vinitpatel135@users.noreply.github.com>
2024-10-04 05:29:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Home from './components/Home';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
